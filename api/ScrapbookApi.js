fc4f83cd37eecde87e3963ec40dfb92948da95b9
eagraf <graf.ethan@gmail.com>
2017-01-29 17:43:19 -0500

    Implemented login api call.

--- api/ScrapbookApi.js
+++ api/ScrapbookApi.js
@@ -7,9 +7,25 @@
 
 import ApiUtils from '../utilities/ApiUtils';
 
+var apiUrl = 'http://54.174.232.52';
+
 var ScrapbookApi = {
-    login: (email, password) => {
-        console.log('Logging in ' + email + ' with password ' + password);
+    // Essentially just get a token for the app to use.
+    login: (userEmail, userPassword) => {
+        fetch(apiUrl + '/auth/token', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: userEmail,
+                password: userPassword
+            })
+        })
+        .then(ApiUtils.checkStatus)
+        .then(response => response.json())
+        .catch(err => err)
     },
 };
 
