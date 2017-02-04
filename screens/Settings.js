7339d99f3079834df23e35aee4a6a9ed95672828
dajin <dajinchu@gmail.com>
2017-02-04 17:02:58 -0500

    Added settings in place of logout in drawer

---
+++ screens/Settings.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  View,
+  ListView,
+} from 'react-native';
+import SettingsList from 'react-native-settings-list';
+
+export default class Settings extends React.Component {
+
+    static navigationOptions = {
+        title: 'Settings',
+        drawer: () => ({
+            label: 'Settings',
+        }),
+    }
+
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
+                    <SettingsList.Item
+                        hasNavArrow={false}
+                        title='Sign Out'
+                        onPress={this.logout}/>
+                </SettingsList>
+            </View>
+        );
+    }
+
+    logout = () => {
+        this.props.navigation.navigate('Login');
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 10,
+    },
+});
